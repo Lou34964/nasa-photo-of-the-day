c377ec7041252d65d35a88ac78e776e4c71ded6f
Chad.Snyder <Mapletonkid@gmail.com>
2019-12-12 16:49:54 -0700

    Day two complete

--- src/Comps/ImgCard.js
+++ src/Comps/ImgCard.js
@@ -1,16 +1,49 @@
 import React from 'react';
+import styled from "styled-components";
 
+const Card = styled.div`
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center
+    aline-content: center;
+    margin-right: auto;
+    margin-left: auto;
+    background-color: translucent;
+    color: white;
+`;
 
+const Title = styled.h2`
+    font-size: 30px;
+    color: coral;
+    background: ocacity: .5;
+`;
+
+const Date = styled.span`
+    font-size: 20px;
+`;
+
+const Img = styled.img`
+    margin-right: auto;
+    margin-left: auto;
+    width: 100%;
+    border-radius: 60px;
+`;
+
+const Explanation = styled.p`
+    font-size: 18px;
+    margin: 3%;
+`;
 
 
 const ImgCard = props=>{
     return(
-        <div className="card-container">
-            <h2>{props.title}</h2>
-            <span>{props.date}</span>
-            <img alt="nasas pic of the day" src={props.url} />
-            <p>{props.explanation}</p> 
-        </div>
+        <Card className="card-container">
+            <Title>{props.title}</Title>
+            <Date>{props.date}</Date>
+            <Img alt="nasas pic of the day" src={props.url} />
+            <Explanation>{props.explanation}</Explanation> 
+        </Card>
     )
 }
 
